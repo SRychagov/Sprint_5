1ff430eed70236014cca9ce4f3a83a44b7a70ebd
Sergey Rychagov <ryc-sergei@yandex.ru>
2024-06-03 17:19:53 +0300

    Добавлен в коммит обновленный файл conftest.py

--- conftest.py
+++ conftest.py
@@ -1,8 +1,10 @@
 import pytest
 from selenium import webdriver
+from data import UrlPage
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+
 
 @pytest.fixture()
 def driver():
@@ -10,6 +12,6 @@ def driver():
     options = Options()
     driver = webdriver.Chrome(service=service, options=options)
     options.add_argument('--window-size=1920,1080')
-    driver.get("https://stellarburgers.nomoreparties.site/")
+    driver.get(UrlPage.main_page)
     yield driver
     driver.quit()
